5da77d1db0951c48283d09321b6f6c6db81ef5c4
randbw <apbward@gmail.com>
2015-05-11 15:01:58 +1000

    made a move function which changes turn after each move

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-  
-  
+
+  // determines whether it is X's or O's go
+  var xTurn = true;
+
+  // In charge of placing X or O in div and then switching turn
+  var move = function () {
+    if (xTurn) {
+      $(this).html('X');
+      xTurn = false;
+    } else {
+      $(this).html('O');
+      xTurn = true;
+    }
+  };
+
+  $('.gameBoard').on('click','.square',move);
+ 
+ 
 });
