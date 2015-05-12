b4ebabb2ad9e1dfc53f9dc9a19ef5edb6b3a03f1
randbw <apbward@gmail.com>
2015-05-13 09:23:12 +1000

    restart button finished. everything working. next: AI

--- js/main.js
+++ js/main.js
@@ -31,52 +31,47 @@ $(document).ready(function(){
   // Decides if game is ended
   var gameOver = false;
 
-  // stores game winner
-  var winrar = '';
+  // stores win amounts
+  var xWins = 0;
+  var oWins = 0;
+
+  // Adds win amount to UI
+  var xAdd = function () {
+    $('#xWins').html('<p>X has ' + xWins + ' wins now.</p>');
+  };
+  var oAdd = function () {
+    $('#oWins').html('<p>O has ' + oWins + ' wins now.</p>');
+  };
 
   // Recognises if there is a winner at end of move
-  var isThereWinner = function () {
-    if ((pandorasBox[0][0] === pandorasBox[1][0] && pandorasBox[1][0] === pandorasBox[2][0]) && (pandorasBox[0][0] === 'X' || pandorasBox[0][0] === 'O')) {
-        alert(pandorasBox[0][0] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[0][0] + ' is the winner!');
+var isThereWinner = function () {
 
-    } else if ((pandorasBox[0][1] === pandorasBox[1][1] && pandorasBox[1][1] === pandorasBox[2][1]) && (pandorasBox[0][1] === 'X' || pandorasBox[0][1] === 'O')) {
-        alert(pandorasBox[0][1] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[0][1] + ' is the winner!');
+  var winningCombos = [['one','two','three'],['four','five','six'],['seven','eight'
+  ,'nine'],['one','four','seven'],['two','five','eight'],['three','six','nine'],['one','five','nine'],['three','five','seven']];
 
-    } else if ((pandorasBox[0][2] === pandorasBox[1][2] && pandorasBox[1][2] === pandorasBox[2][2]) && (pandorasBox[0][2] === 'X' || pandorasBox[0][2] === 'O')) {
-        alert(pandorasBox[0][2] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[0][2] + ' is the winner!');
+  for (var i = 0; i < winningCombos.length; i += 1) {
 
-    } else if ((pandorasBox[0][0] === pandorasBox[0][1] && pandorasBox[0][1] === pandorasBox[0][2]) && (pandorasBox[0][0] === 'X' || pandorasBox[0][0] === 'O')) {
-        alert(pandorasBox[0][0] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[0][0] + ' is the winner!');
+    var cell1 = $('#' + winningCombos[i][0]).html();
+    var cell2 = $('#' + winningCombos[i][1]).html();
+    var cell3 = $('#' + winningCombos[i][2]).html();
 
-    } else if ((pandorasBox[1][0] === pandorasBox[1][1] && pandorasBox[1][1] === pandorasBox[1][2]) && (pandorasBox[1][0] === 'X' || pandorasBox[1][0] === 'O')) {
-        alert(pandorasBox[1][0] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[1][0] + ' is the winner!');
-
-    } else if ((pandorasBox[2][0] === pandorasBox[2][1] && pandorasBox[2][1] === pandorasBox[2][2]) && (pandorasBox[2][0] === 'X' || pandorasBox[2][0] === 'O')) {
-        alert(pandorasBox[2][0] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[2][0] + ' is the winner!');
-
-    } else if ((pandorasBox[0][0] === pandorasBox[1][1] && pandorasBox[1][1] === pandorasBox[2][2]) && (pandorasBox[0][0] === 'X' || pandorasBox[0][0] === 'O')) {
-        alert(pandorasBox[0][0] + ' is the winner!');
-        gameOver = true;
-        $('div.messageBoard').append('<p>' + pandorasBox[0][0] + ' is the winner!');
-
-    } else if ((pandorasBox[0][2] === pandorasBox[1][1] && pandorasBox[1][1] === pandorasBox[2][0]) && (pandorasBox[0][2] === 'X' || pandorasBox[0][2] === 'O')) {
-      alert(pandorasBox[0][2] + ' is the winner!');
+    if ((cell1 === cell2 && cell2 === cell3) && (cell1 === 'X' || cell1 === 'O')) {
+      console.log(cell1 + ' is the winner!');
+      alert(cell1 + ' is the winner!');
       gameOver = true;
-      $('div.messageBoard').append('<p>' + pandorasBox[0][2] + ' is the winner!');
+      $('div.messageBoard').append('<p>' + cell1 + ' is the winner!</p>');
+
+      if (cell1 === 'X') {
+        xWins += 1;
+        xAdd();
+      } else if (cell1 === 'O') {
+        oWins += 1;
+        oAdd();
+      }
+
     }
-  };
+  }
+};
 
   // changes depending who went last to see if it is X's or O's go
   var xTurn = true;
@@ -165,8 +160,22 @@ $(document).ready(function(){
     gamesPlayedUpdateAllowed = true;
   };
 
+  // Restart board function
+  var restartGame = function () {
+    resetBoardFn();
+    xWins = 0;
+    xAdd();
+    oWins = 0;
+    oAdd();
+    xTurn = true;
+    gamesPlayed = 0;
+    gamesCounter();
+    showGo();
+  };
+
   // Activates the move function when squares are clicked
   $('.gameBoard').on('click','.square',move);
   $('#resetBoard').on('click',resetBoardFn);
+  $('#restartGame').on('click',restartGame);
  
 });
